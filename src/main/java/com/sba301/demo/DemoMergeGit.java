a0cb31887210cf4a2420bf99b521e136cafe1aba
DONG-TRI\Admin <thanhminhtriong@gmail.com>
2025-09-10 16:41:41 +0700

    new 1

--- src/main/java/com/sba301/demo/DemoMergeGit.java
+++ src/main/java/com/sba301/demo/DemoMergeGit.java
@@ -8,5 +8,6 @@ public class DemoMergeGit {
         System.out.println("More changes from feature branch.");
         System.out.println("Final changes from feature branch.");
         System.out.println("Changes from main branch.");
+        System.out.println("Additional changes from main branch.");
     }
 }
