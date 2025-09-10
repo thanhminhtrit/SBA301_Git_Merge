3998381585ce8ccd1213dfe494eade3c67db4dc8
DONG-TRI\Admin <thanhminhtriong@gmail.com>
2025-09-10 16:20:46 +0700

    first commit

---
+++ src/main/java/com/sba301/demo/DemoMergeGit.java
@@ -0,0 +1,11 @@
+package com.sba301.demo;
+
+public class DemoMergeGit {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is a demo for merging in Git.");
+        System.out.println("Feature branch changes.");
+        System.out.println("More changes from feature branch.");
+        System.out.println("Final changes from feature branch.");
+    }
+}
---
+++ src/test/java/com/sba301/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sba301.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
