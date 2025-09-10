f71c4003c8438fa2b40e8e0144025e1ac1175acc
DONG-TRI\Admin <thanhminhtriong@gmail.com>
2025-09-10 16:43:43 +0700

    new commit

--- src/main/java/com/sba301/demo/DemoMergeGit.java
+++ src/main/java/com/sba301/demo/DemoMergeGit.java
@@ -8,5 +8,10 @@ public class DemoMergeGit {
         System.out.println("More changes from feature branch.");
         System.out.println("Final changes from feature branch.");
         System.out.println("Changes from main branch.");
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+        }
+        System.out.println("Sum of first 10 natural numbers is: " + sum);
     }
 }
